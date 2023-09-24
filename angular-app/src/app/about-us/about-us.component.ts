90a1de9a1d0b0656b6edf72fbbd2b7ddc357a4b0
Soundhara Pandiyan <pandiyan.k@mksvision.com>
2023-09-24 21:41:10 +0530

    first commit

---
+++ angular-app/src/app/about-us/about-us.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent {
+
+  constructor(private router: Router){}
+
+  onClick(){
+    this.router.navigate(['/contact-us'])
+  }
+
+}
---
+++ angular-app/src/app/events/events.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent {
+
+  constructor(private router: Router){}
+
+  onClick(){
+    this.router.navigate(['/contact-us'])
+  }
+}
